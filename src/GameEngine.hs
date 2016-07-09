bd143ce272c047d8dbf6f163887e6ebc0556d8bc
Martin Galese <martin@galese.net>
2016-07-08 20:55:48 -0400

    rename GameM

--- src/GameEngine.hs
+++ src/GameEngine.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE ImplicitParams #-}
 
 module GameEngine where
 
@@ -9,7 +10,7 @@ import qualified Control.Monad.Random as Random
 import Control.Monad.Reader as Reader
 import qualified Control.Monad.State as State
 import qualified Data.IntMap.Strict as IntMap
-import Data.Maybe (fromJust)
+import Data.Maybe (fromJust, mapMaybe)
 
 
 import Prelude hiding (Either(..), id, (.))
@@ -17,6 +18,9 @@ import Coord
 import Entity
 import GameState
 import GameMap
+import UI
+import Lighting
+import Symbol
 
 
 newtype GameEngine a = GameEngine
@@ -71,3 +75,23 @@ entitiesAt :: Coord -> GameEngine [Entity]
 entitiesAt coord = do
     state <- ask
     return $ (unpackEntityMap $ (unsafeGetCachedMap state) ^. entityMap) <!> coord
+
+render :: DisplayContext -> GameState -> VisibleMap -> IO ()
+render display state visibleMap = let ?context = display in do
+    clearWindow display
+    centerViewOn (state ^. playerPosition)
+    let visible e = ((unpackVisibleMap visibleMap) <!> (e ^. position)) || (isPlayerEntity e)
+        visibleEntities = filter visible (levelEntities (state ^. currLevel))
+        litEntities = lightEntities visibleEntities (state ^. currLevel)
+    mapM_ putEntity $ litEntities
+    drawDisplay display
+
+lightEntities :: [Entity] -> LevelState -> [Entity]
+lightEntities entities lstate = mapMaybe lightEntity entities where
+  cache = lstate ^. (cachedMap . unsafeFromJust)
+  lightingMap = unpackLightMap $ cache ^. lightMap
+  lightEntity e = if aboveThreshold 0.25 ll
+                  then Just $ e & (symbol . baseColor) %~ (apparentColor ll)
+                  else Nothing
+              where
+                ll = lightingMap <!> (e ^. position)
